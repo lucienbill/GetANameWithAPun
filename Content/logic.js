3d496fb91fc6fde99586e8b59f27d7a0822a7e67
Lucien Bill <lucien.bill@gmail.com>
2021-10-22 12:49:33 +0200

    First Commit
    This is the english version of my site https://nomavecjeudemotsdebile.pages.dev/

---
+++ Content/logic.js
@@ -0,0 +1,46 @@
+// --- Global variables ---
+// set timer identifier for the thing that handles how the copy button looks
+timerID = null;
+// Stuff for the thing that handles how the copy button looks
+copyButtonLooks = {
+    "copy"   : "Copy",
+    "copied" : "Copied ✔"
+}
+
+// --- Functions ---
+function printARandomName(list) {
+    document.getElementById("fname").value = list[Math.floor(Math.random() * list.length)];
+}
+
+function copyToClipboard() {
+    /* Get the text field */
+    var copyText = document.getElementById("fname");
+
+    /* Select the text field */
+    copyText.select();
+    copyText.setSelectionRange(0, 99999); /* For mobile devices */
+
+    /* Copy the text inside the text field */
+    navigator.clipboard.writeText(copyText.value);
+
+    /* Notify the user that "copy" is done */
+    cpButton = document.getElementById("copyToClipboard")
+    cpButton.innerHTML = copyButtonLooks.copied;
+    // Cancel the timer if it exists
+    if (timerID !== null)
+    {
+        clearTimeout(timerID);
+    }
+    // Set a timer to reset the button to "copy" after a while
+    timerID = setTimeout(
+        function(){
+            cpButton.innerHTML = copyButtonLooks.copy;
+        }
+        , 2000
+    )
+}
+
+// --- Do stuff ---
+
+// print a random name on page landing
+printARandomName(names);
---
+++ README.md
@@ -0,0 +1,24 @@
+# Website to get a random name with a play on words
+## What's this?
+I made a website that spits out a random element from a list of names with plays on words.
+I first made a [french version](https://nomavecjeudemotsdebile.pages.dev/), then I made [this english version](https://getanamewithapun.pages.dev/).
+
+This repo contains the source code of the english version.
+
+## How does it work?
+### The website
+The website chooses for you a random element from a list of names with plays on words.
+The list is in Content/names.js.
+### Hosting
+I ude [CloudFlare Pages](https://pages.cloudflare.com/#pricing).
+The source code of the website is in the "Content" folder (vanilla HTML + JS and a bit of CSS) ; it is 100% front-end.
+The deployment process is: CloudFlare copies what's in the "Content" folder on the server, and that's basicaly it.
+(Well, it's a bit more subtle than that, but you get the Idea. Read the official CloudFlare documentation to know more.)
+
+## Can I participate?
+Yes, you can!
+If you want to add or modify anything (some names, some CSS, some fancy stuff), just submit an issue or a PR.
+
+### I want to code, are there guidelines?
+The (arbitrairy) guidelines are: no duplicates, and keep an alphabetical order.
+And... that's it.
